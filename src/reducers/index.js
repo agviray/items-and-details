767eafb94678645acbf1809cf6c34d5b70c388b7
Allan Viray <allang.v.dev@gmail.com>
2022-04-24 17:32:19 -0500

    Create reducers directory and first reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,28 @@
+// **************
+// Reducers
+// **************
+
+// - Will temporarily return a static list of songs 
+//   and song details for testing purposes.
+const itemsReducer = () => {
+  return [
+    { title: 'Glycerine', duration: '4:19'},
+    { title: 'No Diggity', duration: '4:15'},
+    { title: 'Black Hole Sun', duration: '5:19'},
+    { title: 'I Want It That Way', duration: '3:34'},
+    { title: 'Hypnotize', duration: '3:51'}
+  ];
+};
+
+// - Takes in a selected item and action object as 
+//   arguments.
+
+// - IMPORTANT - selectedItem is given the value of null. This  
+//   is because selectedItem will require an initial value when
+//   when our app first starts up.
+const selectedItemReducer = (selectedItem = null, action) => {
+  if (action.type === 'ITEM_SELECTED') {
+    return action.payload;
+  }
+  return selectedItem;
+};
